31c839004b35c412b3d1327a8937b7b87cb8abf4
LookerClocker <sahakushnir@gmail.com>
2016-11-18 14:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,3 @@
-// jQuery(document).ready(function () {
-//     jQuery.ajax({
-//         type: 'GET',
-//         headers: {'X-Parse-Application-Id': 'promoSite', 'X-Parse-REST-API-Key': 'J59-8zpY7r3~Vf/t'},
-//         url: "http://server.mon-coupon.com/parse/classes/Coupon",
-//         data: 'include=retailer,category',
-//         contentType: 'text/javascript',
-//         error: function (e) {
-//             alert('Error ' + e.responseText);
-//             console.log(e);
-//         }
-//     }).done(function (resultObject, status) {
-//         var coupon = resultObject.results;
-//         if (coupon && coupon.length > 0) {
-//             var ul = jQuery("<ul id='col-one'></ul>");
-//             for (var i = 0; i < coupon.length; i++) {
-//                 var li = null;
-//                 if (coupon[i].objectId && coupon[i].objectId.length > 0) {
-//                     var description = "<p>Description: " +coupon[i].description;"</p>";
-//                     var code = "<p>Code: " +coupon[i].code;"</p>";
-//                     var rate = "<p>Rate: " +coupon[i].rate;"</p>";
-//                     var uses = "<p>Uses: " +coupon[i].uses;"</p>";
-//                     var type = "<p>Coupon type: " +coupon[i].type;"</p>";
-//                     var retailerName = "<p>Retailer name: " +coupon[i].retailer.name;"</p>";
-//                     var retailerImage ='<img src="'+coupon[i].retailer.image.url+'" alt="logo">';
-//                     var categoryName ="<p>Category name: " +coupon[i].category.categoryName;"</p>";
-//                     var categoryType ="<p>Category type: " +coupon[i].category.type;"</p>";
-//                     var createdDate = "<p>Created at: " + new Date(coupon[i].createdAt).toLocaleDateString();"</p>";
-//                     var expirationDate = "<p>Expiration date: " + new Date(coupon[i].expirationDate.iso).toLocaleDateString();"</p>";
-//                     var line = "<hr/>";
-//
-//                     li = jQuery("<li></li>").append(retailerImage, retailerName, code, description, rate, uses, type, categoryName, categoryType, createdDate, expirationDate, line);
-//                 } else {
-//                     li = jQuery("<li></li>").append(code, description, rate, uses, type, retailerName, retailerImage, categoryName, categoryType, createdDate, expirationDate, line);
-//                 }
-//                 ul.append(li);
-//             }
-//             jQuery("#coupon").append(ul);
-//         }
-//     });
-// });
-
 jQuery(document).ready(function () {
     jQuery.ajax({
         type: 'GET',
@@ -59,7 +17,7 @@ jQuery(document).ready(function () {
                 var li = null;
                 if (coupon[i].objectId && coupon[i].objectId.length > 0) {
                     var mainDiv = document.createElement('div');
-                    document.getElementsByTagName('body')[0].appendChild(mainDiv);
+                    document.getElementById('coupon')[0].appendChild(mainDiv);
                     var couponHeader = document.createElement('div');
                     var couponTitle = document.createElement('span');
                     var usesSpan = document.createElement('span');
